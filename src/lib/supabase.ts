c540a530e7f3a2da71ef63c11e7bed68f472f104
auliagervanojuniormeda-jpg <auliagervanojuniormeda@gmail.com>
2026-09-23 16:05:08 +0800

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://qegwmtucrpukboretfdo.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_r6DiNItrVyzdOcHhCFx7XQ_BuT8qou0";
+const SUPABASE_URL = "https://kkfcgwnrdpmdkjbiixno.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_63e4WGR1IRsl5Ugy3MKwnA_iT5cETPb";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
